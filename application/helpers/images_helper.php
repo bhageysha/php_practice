1baeeeddf42f5fdb7461efa369e3b1e4c25854c1
Gurpreet Singh <gurpreet2501@gmail.com>
2019-07-01 17:42:25 +0530

    changes added

--- application/helpers/images_helper.php
+++ application/helpers/images_helper.php
@@ -4,6 +4,7 @@ function getImage($cont_name){
 	$images = [
 		'admin' => base_url().'/assets/images/bg.jpg',
 		'patient' => base_url().'/assets/images/banner1.jpg'
+		'doctor' => base_url().'/assets/images/banner1.jpg'
 	];
 
 	return $images[$cont_name];
